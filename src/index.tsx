9da53ba85709a92aa4ce27f146774003da2d0083
Maurizio Carella <maurizio.carella27@gmail.com>
2021-05-10 20:46:44 +0200

    v1.2

--- src/index.tsx
+++ src/index.tsx
@@ -25,26 +25,28 @@ export interface TranslateProviderProps {
 	defaultLocale?: string
 }
 
+export interface WithTranslateProps {
+	translate: string
+}
+
 const TranslateContext = React.createContext<TranslateProviderProps>({
 	messages: {},
 	locale: 'en'
 })
 
-export const TranslateProvider: React.FC<TranslateProviderProps> = ({children, ...props}) => {
-	return (
-		<TranslateContext.Provider value={props} children={children}/>
-	)
+export const TranslateProvider: React.FC<TranslateProviderProps> = ({ children, ...props }) => {
+	return <TranslateContext.Provider value={props} children={children} />
 }
 
 export const Translate = ({ id, values = {} }: TranslateProps) => {
 	const translate = useTranslate()
 
-	return <span dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(translate(id, values)) }}/>
+	return <span dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(translate(id, values)) }} />
 }
 
 export const useTranslate = () => {
 	return (id: TranslateID, values: TranslateValues = {}) => {
-		const {messages, locale, defaultLocale} = useContext(TranslateContext)
+		const { messages, locale, defaultLocale } = useContext(TranslateContext)
 
 		let message = messages[locale][id] || (defaultLocale ? messages[defaultLocale][id] || id : id)
 
@@ -56,16 +58,21 @@ export const useTranslate = () => {
 			const key = m[1]
 			let value = values[key] || ''
 
-			if (React.isValidElement(value))
-				value = ReactDOMServer.renderToStaticMarkup(
-					<TranslateContext.Provider value={{messages, locale, defaultLocale}}>
-						{value}
-					</TranslateContext.Provider>
-				)
+			if (React.isValidElement(value)) value = ReactDOMServer.renderToStaticMarkup(<TranslateContext.Provider value={{ messages, locale, defaultLocale }}>{value}</TranslateContext.Provider>)
 
 			message = message.replaceAll(replace, value)
 		}
 
 		return message
 	}
+}
+
+export const withTranslate = <T extends WithTranslateProps>(WrappedComponent: React.ComponentType<T>) => {
+	const ComponentWithExtraInfo = (props: Omit<T, keyof WithTranslateProps>) => {
+		const translate = useTranslate()
+
+		// At this point, the props being passed in are the original props the component expects.
+		return <WrappedComponent {...(props as T)} translate={translate} />
+	}
+	return ComponentWithExtraInfo
 }
