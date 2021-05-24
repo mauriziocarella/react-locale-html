5c874e745a124e62f7ba8119d9b821d4d42a867a
Maurizio Carella <maurizio.carella27@gmail.com>
2021-05-24 17:08:47 +0200

    v1.2.8: improved values key (dotted). fix multiple values replacement

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React, { useContext } from 'react'
 import ReactDOMServer from 'react-dom/server'
 // @ts-ignore
 import DOMPurify from 'dompurify'
+import { get } from './utils'
 
 const regex = /{{([\s\S]+?)}}/g
 
@@ -43,7 +44,7 @@ export const TranslateProvider: React.FC<TranslateProviderProps> = ({ children, 
 export const Translate = ({ id, values = {} }: TranslateProps) => {
 	const translate = useTranslate()
 
-	return <span dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(translate(id, values)) }} />
+	return <span dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(translate(id, values), { USE_PROFILES: { html: true }, ADD_ATTR: ['target'] }) }} />
 }
 
 export const useTranslate = () => {
@@ -53,14 +54,20 @@ export const useTranslate = () => {
 		let message = messages[locale][id] || (defaultLocale ? messages[defaultLocale][id] || id : id)
 
 		if (message) {
+			const org = message
+
 			let m
-			while ((m = regex.exec(message)) !== null) {
+			while ((m = regex.exec(org)) !== null) {
+				if (m.index === regex.lastIndex) {
+					regex.lastIndex++
+				}
+
 				const replace = m[0]
 				const key = m[1]
-				let value = values[key] || ''
 
-				if (React.isValidElement(value)) value = ReactDOMServer.renderToStaticMarkup(<TranslateContext.Provider
-					value={{ messages, locale, defaultLocale }}>{value}</TranslateContext.Provider>)
+				let value = get(values, key) || ''
+
+				if (React.isValidElement(value)) value = ReactDOMServer.renderToStaticMarkup(<TranslateContext.Provider value={{ messages, locale, defaultLocale }}>{value}</TranslateContext.Provider>)
 
 				message = message.replace(new RegExp(replace, 'g'), value)
 			}
@@ -86,7 +93,7 @@ export const formatMessage = (message: MessageID, values: MessageValues) => {
 		while ((m = regex.exec(message)) !== null) {
 			const replace = m[0]
 			const key = m[1]
-			const value = values[key] || ''
+			const value = get(values, key) || ''
 
 			message = message.replace(new RegExp(replace, 'g'), value)
 		}
